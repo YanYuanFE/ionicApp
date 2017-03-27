0d8f8b4e6b1fa70b4f666c1e3f441ab6523fb4b0
YanYuanFE <1025138284@qq.com>
2017-03-27 22:04:17 +0800

    add ble

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { DevicePage } from '../device/device';
+import { BLE } from 'ionic-native';
+
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
@@ -9,7 +12,34 @@ import { NavController } from 'ionic-angular';
 export class AboutPage {
 
   constructor(public navCtrl: NavController) {
+    this.devices = [];
+    this.isScanning = false;
 
+  }
+
+  startScanning() {
+    console.log("Scaning Started");
+    this.devices = [];
+    this.isScanning = true;
+    BLE.startScan([]).subscrible(device => {
+      this.devices.push(device);
+    });
+
+    setTimeout(() => {
+      BLE.stopScan().then(() => {
+        console.log("Scanning has stopped");
+        console,log(JSON.stringify(this.devices));
+        this.isScanning = false;
+      });
+    },3000);
+  }
+
+  connectToDevice(device) {
+    console.log("Connect To Device");
+    console.log(JSON.stringify(device));
+    this.navCtrl.push(DevicePage,{
+      device:device
+    });
   }
 
 }
